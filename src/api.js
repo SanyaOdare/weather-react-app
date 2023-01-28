b6dc5c77966503f293f1baa6c3a9bc0b438a77be
SanyaOdare <sanyaodare@gmail.com>
2023-01-28 13:26:44 +0100

    populate weather card component

---
+++ src/api.js
@@ -0,0 +1,16 @@
+// a02057c7789cbbaf775719bed27c552f
+import axios from "axios";
+
+export async function fetchWeather(city, setError) {
+  console.log(process.env.REACT_APP_WEATHER_API_KEY);
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
+
+  try {
+    const response = await axios.get(url);
+    setError("");
+    return response.data
+  } catch (error) {
+    setError("City Not Found!");
+    return error;
+  }
+}
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -5,7 +5,23 @@ const WeatherCard = ({ weather }) => {
     <div className='Weather-card'>
       <div className='Card-header'>
         <div>
-          <p></p>
+          <p className='Weather-city'>
+            {weather ? weather.name + " " + weather.sys.country : "-"}</p>
+          <p className='Weather-description'>
+          {weather ? weather.weather[0].description : "-"}</p>
+        </div>
+        <img 
+          alt={'weather'} className={'Weather-icon'} 
+          src={`icons/${weather ? weather.weather[0].icon : "unknown"}.png`}
+        />
+      </div>
+      <div className='Card-body'>
+        <h2 className='Weather-temp'>{weather ? Math.round(weather.main?.temp - 273.15) : ""}°C</h2>
+        <div>
+          <div>
+            <span></span>
+          </div>
+
         </div>
       </div>
     </div>
